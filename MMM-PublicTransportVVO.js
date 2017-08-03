ced63a9d72fe533ad1f19e9936d7d077a163312a
ChristianGeie <cg909@web.de>
2017-08-04 00:36:59 +0200

    initial commit

---
+++ MMM-PublicTransportVVO.js
@@ -0,0 +1,337 @@
+Module.register("MMM-PublicTransportVVO", {
+
+  // default values
+  defaults: {
+    name: "MMM-PublicTransportVVO",
+    hidden: false,
+    stationId: 33000313,  // [[["Dresden"]],[["Räcknitzhöhe","Dresden","33000313"]]]
+//    stationId: 33000037,
+//    stationId: 33006770,  // [[["Arnsdorf b Dresden"]],[["Bahnhof","Arnsdorf b Dresden","33006770"]]]
+//    stationId: 33000841,   // [[["Dresden"]],[["Scharfensteinstraße","Dresden","33000841"]]]
+    baseurl: "http://widgets.vvo-online.de/abfahrtsmonitor/",
+    stationuri: "Haltestelle.do?hst=",
+    departureuri: "Abfahrten.do?hst=",
+    colored: false,                       // show not reachable departures colored
+    animationSpeed: 1 * 1000,             // 1 sec
+    updateInterval: 30 * 1000,            // 30 sec
+
+    fade: true,
+    fadePoint: 0.25,                      // start on 1/4th of the list.
+
+    initialLoadDelay: 0,
+    retryDelay: 2500,                     // if request fails, do a retry after 2.5 sec
+
+    marqueeLongDirections: true,
+    delay: 10,                             // How long do you need to walk to the next Station?
+    showTableHeaders: true,               // show location and station in table header
+    showTableHeadersAsSymbols: false      // table headers as symbols or written?
+  },
+	// create some variables to hold the station name and city based on the API result.
+	fetchedStationCity: "",
+  fetchedStationName: "",
+
+  start: function () {
+    Log.info("Starting module: " + this.name);
+
+    this.departuresArray = [];
+    this.loaded = false;
+
+    this.scheduleStationUpdate(this.config.initialLoadDelay);
+    this.scheduleDepartureUpdate(this.config.initialLoadDelay);
+
+    this.updateTimer = null;
+
+    // check some variables
+    if(this.config.delay < 0) {
+      this.config.delay = 0;
+    }
+
+    if (this.config.updateInterval < 30000) {
+      this.config.updateInterval = 30000;
+    }
+  },
+
+  getDom: function () {
+
+  let wrapper = document.createElement("div");
+  wrapper.className = "ptbWrapper";
+
+	if (this.config.stationId === "") {
+  	wrapper.innerHTML = "Please set the correct stationID: " + this.name + ".";
+	 	wrapper.className = "dimmed light small";
+	  return wrapper;
+	}
+
+  if (this.departuresArray.length === 0 && !this.loaded) {
+    wrapper.innerHTML = (this.loaded) ? this.translate("EMPTY") : this.translate("LOADING");
+    wrapper.className = "small light dimmed";
+    return wrapper;
+  }
+
+  let heading = document.createElement("header");
+  heading.innerHTML = this.fetchedStationCity + ", "+ this.fetchedStationName
+  wrapper.appendChild(heading);
+
+  // table header
+  let table = document.createElement("table");
+  table.className = "ptbTable small light";
+
+  if (this.config.showTableHeaders) {
+    let tHead = document.createElement("thead");
+
+    let headerRow = document.createElement("tr");
+
+    // Header Cell for line symbol
+    let headerLine = document.createElement("td");
+
+    if (this.config.showTableHeadersAsSymbols) {
+      headerLine.className = "centeredTd";
+      let lineIcon = document.createElement("span");
+      lineIcon.className = "fa fa-tag";
+      headerLine.appendChild(lineIcon);
+    } else {
+      headerLine.innerHTML = "Linie";
+    }
+
+    headerRow.appendChild(headerLine);
+
+    // Header Cell for direction
+    let headerDirection = document.createElement("td");
+
+    if (this.config.showTableHeadersAsSymbols) {
+      headerDirection.className = "centeredTd";
+      let directionIcon = document.createElement("span");
+      directionIcon.className = "fa fa-exchange";
+      headerDirection.appendChild(directionIcon);
+    } else {
+      headerDirection.innerHTML = "Nach";
+    }
+
+    headerRow.appendChild(headerDirection);
+
+    // Header Cell for departure time
+    let headerTime = document.createElement("td");
+
+    if (this.config.showTableHeadersAsSymbols) {
+      headerTime.className = "centeredTd";
+      let timeIcon = document.createElement("span");
+      timeIcon.className = "fa fa-clock-o";
+      headerTime.appendChild(timeIcon);
+    } else {
+      headerTime.innerHTML = "Abfahrt in min";
+    }
+
+    headerRow.appendChild(headerTime);
+
+    headerRow.className = "bold dimmed";
+    tHead.appendChild(headerRow);
+
+    table.appendChild(tHead);
+    }
+
+    // Create table body
+    let tBody = document.createElement("tbody");
+
+    // Handle empty departures array
+    if (this.departuresArray.length === 0) {
+      let row = this.getNoDeparturesRow("There are currently no departures.");
+
+      tBody.appendChild(row);
+      table.appendChild(tBody);
+      wrapper.appendChild(table);
+      return wrapper;
+    }
+
+    this.departuresArray.forEach((current, i) => {
+    if (i < this.config.maxReachableDepartures) {
+      let row = this.getRow(current);
+
+      // fading out
+      if (this.config.fade && this.config.fadePoint < 1) {
+        if (this.config.fadePoint < 0) {
+          this.config.fadePoint = 0;
+        }
+        var startingPoint = this.departuresArray.length * this.config.fadePoint;
+        var steps = this.departuresArray.length - startingPoint;
+        if (i >= startingPoint) {
+          var currentStep = i - startingPoint;
+          row.style.opacity = 1 - (1 / steps * currentStep);
+        }
+      }
+
+      tBody.appendChild(row);
+      }
+    });
+
+    table.appendChild(tBody);
+
+    wrapper.appendChild(table);
+
+    return wrapper;
+  },
+
+  getRow: function (current) {
+
+    let row = document.createElement("tr");
+
+    // Cell for line
+    let lineCell = document.createElement("td");
+    lineCell.className = "centeredTd noPadding lineCell";
+    lineCell.innerHTML = current.departurenumber;
+    row.appendChild(lineCell);
+
+    let directionCell = document.createElement("td");
+    directionCell.className = "directionCell bright";
+
+    if (this.config.marqueeLongDirections && current.departuredirection.length >= 26) {
+      directionCell.className = "directionCell marquee";
+      let directionSpan = document.createElement("span");
+      directionSpan.innerHTML = current.departuredirection;
+      directionCell.appendChild(directionSpan);
+    } else {
+      directionCell.innerHTML = current.departuredirection;
+    }
+
+    row.appendChild(directionCell);
+
+    let timeCell = document.createElement("td");
+    timeCell.className = "centeredTd timeCell bright";
+    if (this.config.delay > 0 && current.departuretime <= this.config.delay && this.config.colored) {
+      timeCell.style.color = "red";
+    }
+    if (current.departuretime === "") {
+      timeCell.innerHTML = "jetzt";
+    } else {
+      timeCell.innerHTML = current.departuretime;
+    }
+    row.appendChild(timeCell);
+
+    return row;
+  },
+
+  /* scheduleStationUpdate()
+	 * Schedule next station update.
+	 *
+	 * argument delay number - Milliseconds before next update. If empty, this.config.updateInterval is used.
+	 */
+	scheduleStationUpdate: function(delay) {
+		var nextLoad = this.config.updateInterval;
+		if (typeof delay !== "undefined" && delay >= 0) {
+			nextLoad = delay;
+		}
+
+		var self = this;
+
+    setTimeout(function() {
+			self.getStationDetails();
+		}, nextLoad);
+	},
+
+	/* getStationDetails(compliments)
+	 * Requests new data.
+	 * Calls processStationDetails on succesfull response.
+	 */
+  getStationDetails: function () {
+    var url = this.config.baseurl + this.config.stationuri + this.config.stationId;
+    var self = this;
+    var retry = true;
+
+    var StationDetailsRequest = new XMLHttpRequest();
+    StationDetailsRequest.open("GET", url, true);
+    StationDetailsRequest.onreadystatechange = function() {
+    	if (this.readyState === 4) {
+		  	if (this.status === 200) {
+			  	self.processStationDetails(JSON.parse(this.response));
+  			} else {
+				  Log.error(self.name + ": Could not load details.");
+			  }
+
+  			if (retry) {
+	 				self.scheduleStationUpdate((self.loaded) ? -1 : self.config.retryDelay);
+	  		}
+		  }
+    };
+    StationDetailsRequest.send();
+  },
+
+	/* processStationDetails(data)
+   * Uses the received data to set the various values.
+	 *
+   * argument data object - information received from the api.
+   */
+  processStationDetails: function(data) {
+    this.fetchedStationCity = data[0][0][0];
+    this.fetchedStationName = data[1][0][0];
+
+	  this.updateDom(this.config.animationSpeed);
+  },
+
+  /* scheduleDepartureUpdate()
+	 * Schedule next departure update.
+	 *
+	 * argument delay number - Milliseconds before next update. If empty, this.config.updateInterval is used.
+	 */
+  scheduleDepartureUpdate: function(delay) {
+		var nextLoad = this.config.updateInterval;
+		if (typeof delay !== "undefined" && delay >= 0) {
+			nextLoad = delay;
+		}
+
+		var self = this;
+
+    setTimeout(function() {
+			self.getDepartureDetails();
+		}, nextLoad);
+	},
+
+  /* getDepartureDetails(compliments)
+	 * Requests new data.
+	 * Calls processDepartureDetails on succesfull response.
+	 */
+  getDepartureDetails: function () {
+    var url = this.config.baseurl + this.config.departureuri + this.config.stationId;
+    var self = this;
+    var retry = true;
+
+    var DepartureDetailsRequest = new XMLHttpRequest();
+    DepartureDetailsRequest.open("GET", url, true);
+    DepartureDetailsRequest.onreadystatechange = function() {
+    	if (this.readyState === 4) {
+		  	if (this.status === 200) {
+			  	self.processDepartureDetails(JSON.parse(this.response));
+				} else {
+				  Log.error(self.name + ": Could not load details.");
+			  }
+
+				if (retry) {
+  				self.scheduleDepartureUpdate((self.loaded) ? -1 : self.config.retryDelay);
+	  		}
+		  }
+    };
+    DepartureDetailsRequest.send();
+  },
+
+  /* processDepartureDetails(data)
+   * Uses the received data to set the various values.
+	 *
+   * argument data object - information received from the api.
+   */
+  processDepartureDetails: function(data) {
+    this.departuresArray = [];
+
+		for (var i = 0, count = data.length; i < count; i++) {
+			var departuresArray = data[i];
+			this.departuresArray.push({
+				departurenumber: departuresArray[0],
+				departuredirection: departuresArray[1],
+				departuretime: departuresArray[2]
+			});
+		}
+	  this.loaded = true;
+	  this.updateDom(this.config.animationSpeed);
+  },
+
+  getStyles: function () {
+    return ['style.css'];
+  },
+});
---
+++ README.md
@@ -0,0 +1,79 @@
+# MMM-PublicTransportVVO
+
+MMM-PublicTransportVVO is a module for the [MagicMirror](https://github.com/MichMich/MagicMirror) project by
+[Michael Teeuw](https://github.com/MichMich).
+
+It shows live public transport information for Verkehrsverbund Oberelbe (Germany) based on http://widgets.vvo-online.de api data.
+
+
+*Notes:*
+* *The module is working fine. But I think some value combinations could still lead to strange behaviour. I'd appreciate any error report.*
+* *Fading for departures with delay = 0 is not yet implemented*
+
+You can enter a delay time for "How long does it take to get to my station?".
+Then the module calculates the next reachable departures and draws all unreachable departures in a different style or color.
+
+## Screenshot
+
+The module looks like this:
+
+![Example for Dresden, Postplatz with default delay](img/MMM-PublicTransportVVO_screenshot.png)
+
+## Installation
+
+Just clone the module into your MagicMirror modules folder:
+
+```
+git clone https://github.com/ChristianGeie/MMM-PublicTransportVVO.git
+```
+
+## How to get the `stationId`
+
+You will need a `stationId` for your module. *TODO* _Link full list or query target to get station IDs._
+
+## Configuration
+
+The module quite configurable. These are the possible options:
+
+|Option|Description|
+|---|---|
+|`name`|The name of the module instance (if you want multiple modules).<br><br>**Type:** `string`<br>**Default value:** `MMM-PublicTransportVVO`|
+|`stationId`|The ID of the station. How to get the ID for your station is described below.<br><br>**Type:** `integer`<BR>**Default value:** `33000037`<br> This value is **Required**.|
+|`marqueeLongDirections`|Makes a marquee/ticker text out of all direction descriptions with more than 25 characters. If this value is false, the descriptions are trimmed to the station names. If the movement is not fluent enough for you, you should turn it off.<br><br>**Type:** `boolean`<br>**Default value:** `true`|
+|`updateInterval`|How often the module should be updated. The value is given in milliseconds.<br><br>**Type:** `integer`<br>**Default value:** `30000` // 30 seconds|
+|`hidden`|Visibility of the module.<br><br>**Type:** `boolean`<br>**Default value:** `false`|
+|`delay`|How long does it take you to get from the mirror to the station? The value is given in minutes.<br><br>**Type:** `integer`<br>**Default value:** `10` // 10 minutes|
+|`showTableHeaders`|Show the table headers with information about location and station name.<br><br>**Type:** `boolean`<br>**Default value:** `true`|
+|`showTableHeadersAsSymbols`|Show the table headers as text or symbols.<br><br>**Type:** `boolean`<br>**Default value:** `true`|
+
+Here is an example of an entry in `config/config.js`:
+
+```javaScript
+{
+    module: 'MMM-PublicTransportVVO',
+    position: 'top_right',
+    config: {
+        stationId: 33000313,
+        hidden: false,
+        delay: 0,
+        updateInterval: 120000,
+        marqueeLongDirections: false,
+        showColoredLineSymbols: true,  
+        showTableHeadersAsSymbols: true,
+    }
+},
+```
+
+## Multiple Modules
+
+Multiple instances of this module are possible. Just add another entry of the MMM-PublicTransportVVO module to the `config/config.js` of your mirror.
+
+## Special Thanks
+
+* [Michael Teeuw](https://github.com/MichMich) for inspiring me and many others to build a MagicMirror.
+* [Bangee44](https://github.com/Bangee44) for creating the [MMM-swisstransport](https://github.com/Bangee44/MMM-swisstransport) module, on which this one is heavily based.
+* The community of [magicmirror.builders](https://magicmirror.builders) for help in the development process.
+
+## Issues
+
+If you find any problems, bugs or have questions, please [open a GitHub issue](https://github.com/ChristianGeie/MMM-PublicTransportVVO/issues) in this repository.
